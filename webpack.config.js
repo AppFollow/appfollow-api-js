cde9b61567e3621bfdd7e72cce3e1df67817c721
Dmitri Markovskiy <dmitri.markovskiy@gmail.com>
2018-08-05 15:05:59 +0300

    Fix config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
 const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+	cache: true,
+	mode: 'production',
 	output: {
 		filename: 'api-sign.min.js',
 		path: path.resolve(`${__dirname}/dist`),
 	},
 	entry: {
-		'index': './src/api-sign.js',
+		'index': './index.js',
 	},
 	plugins: [
 		new CleanWebpackPlugin('dist'),
-		new UglifyJSPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production'),
+			},
+		}),
 	],
+	optimization: {
+		concatenateModules: true,
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+			}),
+		],
+	},
 	module: {
 		rules: [
 			{
@@ -22,8 +37,8 @@ module.exports = {
 				exclude: /node_modules/,
 				use: {
 					loader: 'babel-loader',
-				}
-			}
-		]
+				},
+			},
+		],
 	},
 };
